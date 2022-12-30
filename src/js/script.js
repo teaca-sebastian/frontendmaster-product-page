76e7ed66e311d1a77b141a2dd95db6b5488f2beb
Teaca Sebastian <sebiteaca@gmail.com>
2022-12-30 21:35:58 +0200

    Add guidelines for handleChange() function

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// Media query for switching cart from dropdown to collapse
+
 const cartButton = document.querySelector('.cart-button')
 const cartDropdown = document.querySelector('.cart-dropdown')
 const cartCollapse = document.querySelector('.cart-collapse')
@@ -17,3 +19,16 @@ var x = window.matchMedia("(max-width: 576px)")
 
 handleCart(x) // Call listener function at run time
 x.addListener(handleCart) // Attach listener function on state changes
+
+// Add active .selected class to carousel thumbnail
+
+const carouselButtons = document.querySelectorAll('[data-bs-target="#productCarousel"')
+const productCarousel = document.querySelector('#productCarousel')
+
+// 1 create handleActive() 
+
+// 1.1 when changing active image in carousel, add .selected to
+// the specific thumbnail
+
+// 1.2 when clicking on one of the thumbanils, it should display
+// on the carousel
