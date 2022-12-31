c5267a9d483f711259c457392fee286241b96011
Teaca Sebastian <sebiteaca@gmail.com>
2022-12-31 09:26:45 +0200

    Finish carousel and modal functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,31 @@
-// Media query for switching cart from dropdown to collapse
+// ### Media query for switching cart from dropdown to collapse
 
 const cartButton = document.querySelector('.cart-button')
 const cartDropdown = document.querySelector('.cart-dropdown')
 const cartCollapse = document.querySelector('.cart-collapse')
 
+const productCarousel = document.querySelector('#productCarousel')
+const carouselThumbnails = document.querySelectorAll('#carouselThumbnailsContainer img')
+// modal variables
+const modalCarousel = document.querySelector('#modalCarousel')
+const modalThumbnails = document.querySelectorAll('#modalThumbnailsContainer img')
+
 function handleCart(x) {
-  if (x.matches) { // If media query matches
+  if (x.matches) {
+     // If media query matches
     cartButton.setAttribute('data-bs-toggle', 'collapse')
     cartButton.setAttribute('data-bs-target', '#cart')
     cartDropdown.classList.remove('show')
+
+    document.querySelectorAll('img').forEach(image => {
+        image.removeAttribute('data-bs-toggle')
+    })
   } else {
+
+    productCarousel.querySelectorAll('img').forEach(image => {
+        image.setAttribute('data-bs-toggle', 'modal')
+    })
+
     cartButton.setAttribute('data-bs-toggle', 'dropdown')
     cartCollapse.classList.remove('show')
   }
@@ -19,16 +35,61 @@ var x = window.matchMedia("(max-width: 576px)")
 
 handleCart(x) // Call listener function at run time
 x.addListener(handleCart) // Attach listener function on state changes
+// i know this is deprecated but it works :)
 
-// Add active .selected class to carousel thumbnail
+// ### Carousel thumbnails and modal carousel scripts:
 
-const carouselButtons = document.querySelectorAll('[data-bs-target="#productCarousel"')
-const productCarousel = document.querySelector('#productCarousel')
+// Pure utility functions:
 
-// 1 create handleActive() 
+function updateThumbnails(thumbnails, index) {
+    // delete .selected class from all
+    thumbnails.forEach(thumbnail => {
+        thumbnail.classList.remove('selected')
+    })
+    // update the one in the arguments
+    thumbnails[index].classList.add('selected')
+}
 
-// 1.1 when changing active image in carousel, add .selected to
-// the specific thumbnail
+function updateCarouselActive(carousel, index) {
+    const carouselItems = carousel.querySelectorAll('.carousel-item')
+    // remove .active from all
+    carouselItems.forEach(item => {
+        item.classList.remove('active')
+    })        
+    // add .active to one with showIndex index
+    carouselItems[index].classList.add('active')
+}
 
-// 1.2 when clicking on one of the thumbanils, it should display
-// on the carousel
+// update carousel and thumbnails on carousel slide
+
+productCarousel.addEventListener('slide.bs.carousel', event => {
+    const index = event.relatedTarget.querySelector('img').dataset.index - 1
+    console.log(event.relatedTarget.querySelector('img'))
+    updateThumbnails(carouselThumbnails, index)
+    updateCarouselActive(modalCarousel, index)
+    updateThumbnails(modalThumbnails, index)
+})
+
+modalCarousel.addEventListener('slide.bs.carousel', event => {
+    const index = event.relatedTarget.querySelector('img').dataset.index - 1
+    updateThumbnails(modalThumbnails, index)
+})
+
+// update carousel and thumbnails on thumbnail click
+
+carouselThumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        updateCarouselActive(productCarousel, index)
+        updateThumbnails(carouselThumbnails, index)
+        updateCarouselActive(modalCarousel, index)
+        updateThumbnails(modalThumbnails, index)
+    })
+})
+
+modalThumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        updateCarouselActive(modalCarousel, index)
+        updateThumbnails(modalThumbnails, index)
+    })
+})
+
